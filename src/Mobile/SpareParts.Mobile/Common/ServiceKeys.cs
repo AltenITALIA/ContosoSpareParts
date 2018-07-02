dde15e3bfc58ba9a41e746f9c8ec685f5fb3c527
Marco Minerva <marco.minerva@gmail.com>
2018-07-02 15:02:20 +0200

    Cancellazione chiavi dal codice
    Related Work Items: #82

--- src/Mobile/SpareParts.Mobile/Common/ServiceKeys.cs
+++ src/Mobile/SpareParts.Mobile/Common/ServiceKeys.cs
@@ -8,7 +8,7 @@ namespace SpareParts.Mobile.Common
 {
     public static class ServiceKeys
     {
-        public const string CustomVisionPredictionKey = "740625682c404203a490c8e49a69b094";
-        public const string CustomVisionProjectId = "7794d59e-f452-4bf5-ae9a-c4a4be42f6a3";
+        public const string CustomVisionPredictionKey = "";
+        public const string CustomVisionProjectId = "";
     }
 }
