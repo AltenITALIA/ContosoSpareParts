94e7725ef007ec97d7467be18255fbe582de3045
nostromo <nostromo_@hotmail.it>
2018-07-02 19:00:42 +0200

    add base service class

--- src/Frontend/src/app/services/parts.service.ts
+++ src/Frontend/src/app/services/parts.service.ts
@@ -4,6 +4,8 @@ import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { BaseService } from './base-service';
+import { AppInsightsService } from '@markpieszak/ng-application-insights';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -14,28 +16,22 @@ const httpOptions = {
 @Injectable({
   providedIn: 'root'
 })
-export class PartsService {
-  constructor(private httpClient: HttpClient) { }
+export class PartsService extends BaseService {
+  constructor(private httpClient: HttpClient,  appInsightsService: AppInsightsService) { super(appInsightsService); }
 
   getParts(): Observable<Part[]> {
     return this.httpClient.get<Part[]>(environment.getPartUrl).pipe(
-      tap(vehicle => console.info('fetch parts'), 
-      catchError(this.handleError('parts', []))
+      tap(vehicle => console.info('fetch parts'),
+        catchError(this.handleError('parts', []))
       )
     );
   }
-addPart(newPart: Part): Observable<ArrayBuffer> {
+  
+  addPart(newPart: Part): Observable<ArrayBuffer> {
     return this.httpClient.post<ArrayBuffer>(environment.addPartUrl, newPart, httpOptions).pipe(
       tap(arrayBuffer => console.info('add new part')),
-        catchError(this.handleError<ArrayBuffer>('cant add part')
+      catchError(this.handleError<ArrayBuffer>('cant add part')
       )
     );
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error); 
-      return of(result as T);
-    };
   }
 }
